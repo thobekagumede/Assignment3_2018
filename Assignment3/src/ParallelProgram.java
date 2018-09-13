9d84b1b840375819d81190e5de28a8273ac6a383
Thobeka Gumede <thobekagumede@gmail.com>
2018-09-13 21:50:27 +0200

    Refactoring code and completed sequential sum of each tree

--- Assignment3/src/ParallelProgram.java
+++ Assignment3/src/ParallelProgram.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class ParallelProgram {
 	public double matrixArray[];
@@ -7,60 +9,65 @@ public class ParallelProgram {
 	public double totalSum = 0;
 	
 	public static void main(String[] args) {
-		File file = new File("sample_input.txt");
+		File file = new File("sample_in.txt");
 		try {
 			BufferedReader buff = new BufferedReader(new FileReader(file));
 			String line = null;
 //			buff.readLine();
 			int counter = 0;
-			String var = null;
+			String dimensionStr = null;
 			String var1 = null;
-			int trees = 0;
-			int[] tree_row = new int[1000000];
-			int[] tree_column = new int[1000000];
-			int[] tree_extent = new int[1000000];
+			int numOfTrees = 0;
+			int[] tree_row = null;
+			int[] tree_column = null;
+			int[] tree_extent = null;
 			
+			// Read in dimensions
+			line = buff.readLine();
+			String[] matrixSize = line.trim().split("\\s+");
+			int rowSize = Integer.parseInt(matrixSize[0]);
+			int columnSize = Integer.parseInt(matrixSize[1]);		
 			
-			while((line=buff.readLine())!=null & line.length() != 0) {				
-				counter++;
-				if(counter == 1) {
-					var = line;
-				}
-				
-				else if(counter == 2) {
-					var1 = line;
-				}
-				
-				else if (counter == 3) {
-					trees = Integer.parseInt(line);
-				}
-				
-				else {
-					tree_row[counter-4] = Integer.parseInt(line.trim().split("\\s+")[0]);
-					tree_column[counter-4] = Integer.parseInt(line.trim().split("\\s+")[1]);
-					tree_extent[counter-4] = Integer.parseInt(line.trim().split("\\s+")[2]);					
-				}
+			// Sunlight values
+			line = buff.readLine();
+			String[] sunlightStringMatrix = line.trim().split("\\s+");
+			double sunlightMatrix[] = new double[rowSize*columnSize]; 
+			for(int i=0; i<rowSize*columnSize; i++) {
+				sunlightMatrix[i] = Double.parseDouble(sunlightStringMatrix[i]);
 			}
 			
-
-
-			String[] matrixSize = var.trim().split("\\s+");
-			String[] matrix = var1.trim().split("\\s+");
-			int row = Integer.parseInt(matrixSize[0]);
-			int column = Integer.parseInt(matrixSize[1]);		
-			double matrixArray[] = new double[row*column]; 
-			for(int i=0; i<row*column; i++) {
-				matrixArray[i] = Double.parseDouble(matrix[i]);
+			// Number of trees
+			line = buff.readLine();
+			numOfTrees = Integer.parseInt(line);
+			
+			// Tree Array
+			Tree [] treeArray = new Tree [numOfTrees];
+			
+			for(int j=0; j<numOfTrees; j++)
+			{
+				line = buff.readLine();
+				String [] treeInfo = line.trim().split("\\s+");
+				int xCoord = Integer.parseInt(treeInfo[0]);
+				int yCoord = Integer.parseInt(treeInfo[1]);
+				int extent = Integer.parseInt(treeInfo[2]);
+				
+				treeArray[j] = new Tree(xCoord, yCoord, extent);
+				
 			}
+			
+			// Done reading from file
+			buff.close();
+			
+			// Sequential implementation
+			
+			computeSequential(numOfTrees, treeArray, rowSize, sunlightMatrix);
+			
+			// Parallel implementation
+			
 			
 //			System.out.println(sunlightSum(0,1,7,row,matrixArray));
 			
-			for (int i=0; i<tree_row.length; i++) {
-				double treeSum = sunlightSum(tree_row[i],tree_column[i],tree_extent[i],row,matrixArray);
-//				totalSum += treeSum;
-				System.out.println(treeSum);
-//				System.out.println(matrixArray[i]);
-			}			
+					
 		} 
 		
 		catch (Exception e) {
@@ -71,13 +78,26 @@ public class ParallelProgram {
 		
 	}
 	
-	private static double sunlightSum(int x, int y, int extent, int dimension, double [] mat) {
+	private static void computeSequential(int numOfTrees, Tree [] treeArray, int rowSize, double [] sunlightMatrix) {
+		for (int i=0; i<numOfTrees; i++) {
+			double treeSum = sunlightSum(treeArray[i],rowSize,sunlightMatrix);
+//			totalSum += treeSum;
+			System.out.println(treeSum);
+//			System.out.println(matrixArray[i]);
+		}	
+		
+	}
+
+	private static double sunlightSum(Tree tree, int dimension, double [] mat) {
+		int x = tree.getxCoord();
+		int y = tree.getyCoord();
+		int extent = tree.getExtent();
 		int x_extent = x + extent;
 		int y_extent = y + extent;
 		double sum = 0.0;
 		int counter = 0;
-		for (int i=x; i<x_extent; i++) {
-			for(int j=y; j<y_extent; j++) {
+		for (int i=x; i<x_extent && i < dimension; i++) {
+			for(int j=y; j<y_extent && j < dimension; j++) {
 				sum += mat[j+i*dimension];
 				counter++;
 			}
